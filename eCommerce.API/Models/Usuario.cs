2024ec961cb4733bfb9d100f3cf14cec08fb5489
Rodrigo Góis <rodrigo.gois@questores.com.br>
2023-02-07 17:58:09 -0300

    Finalizing a User Model class

--- eCommerce.API/Models/Usuario.cs
+++ eCommerce.API/Models/Usuario.cs
@@ -10,5 +10,12 @@ namespace eCommerce.API.Models
         public int Id { get; set; }
         public string Nome { get; set; }
         public string Email { get; set; }
+        public string Sexo { get; set; }
+        public string RG { get; set; }
+        public string CPF { get; set; }
+        public string NomeMae { get; set; }
+        public string SituacaoCadstro { get; set; }
+        public DateTimeOffset DataCadastro { get; set; }
+
     }
 }
