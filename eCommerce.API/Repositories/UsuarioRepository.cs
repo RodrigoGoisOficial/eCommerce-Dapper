519c9f24209d591ad4aab6798c18e47f9581ea12
Rodrigo Góis <rodrigo.gois@questores.com.br>
2023-02-07 16:47:08 -0300

    Add project files.

---
+++ eCommerce.API/Repositories/UsuarioRepository.cs
@@ -0,0 +1,46 @@
+﻿using eCommerce.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerce.API.Repositories
+{
+    public class UsuarioRepository : IUsuarioRepository
+    {
+
+        private static List<Usuario> _db = new List<Usuario>()
+        {
+            new Usuario() { Id=1, Nome="Felipe Rodrigues", Email="felipe.rodrigues@gmail.com" },
+            new Usuario() { Id=2, Nome="Marcelo Rui", Email="marcelo.rui@gmail.com" },
+            new Usuario() { Id=3, Nome="Pricila Rai", Email="pricila.rai@gmail.com" }
+        };
+
+        public List<Usuario> Get()
+        {
+            return _db;
+        }
+
+        public Usuario Get(int id)
+        {
+            return _db.FirstOrDefault(a => a.Id == id);
+        }
+
+        public void Insert(Usuario usuario)
+        {
+            _db.Add(usuario);
+        }
+
+        public void Update(Usuario usuario)
+        {
+            _db.Remove(_db.FirstOrDefault(a => a.Id == usuario.Id));
+            _db.Add(usuario);
+        }
+
+        public void Delete(int id)
+        {
+            _db.Remove(_db.FirstOrDefault(a => a.Id == id));
+        }
+
+    }
+}
